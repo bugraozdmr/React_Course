6b5d9023e7208d21e801b01d1a3d5dce273c581d
bugra <@gmail.com>
2024-06-11 13:49:02 +0300

    Courses

--- Cards/src/App.jsx
+++ Cards/src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from 'react';
-import './App.css';
-import { Course } from './Course';
+import { useState } from "react";
+import "./App.css";
+import { Course } from "./Course";
 
 import Angular from "./images/angular.jpg";
 import Bootstrap from "./images/bootstrap5.png";
 import Csharp from "./images/ccsharp.png";
-
+import "bulma/css/bulma.css";
 
 function App() {
-  
-
   return (
     <>
-      <Course 
-      image = {Angular}
-      title="Angular"
-      description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, consequuntur."/>
+      <section className="hero is-warning">
+        <div className="hero-body">
+          <p className="title">Courses</p>
+          <p className="subtitle">Latest Ones</p>
+        </div>
+      </section>
 
-      <Course 
-      image = {Bootstrap}
-      title="MVC"
-      description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, consequuntur."/>
-      
-      <Course 
-      image = {Csharp}
-      title="Python"
-      description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, consequuntur."/>
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column">
+              <Course
+                image={Angular}
+                title="Angular"
+                description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, consequuntur."
+              />
+            </div>
+
+            <div className="column">
+              <Course
+                image={Bootstrap}
+                title="MVC"
+                description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, consequuntur."
+              />
+            </div>
+
+            <div className="column">
+              <Course
+                image={Csharp}
+                title="Python"
+                description="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, consequuntur."
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Cards/src/Course.jsx
+++ Cards/src/Course.jsx
@@ -11,12 +11,30 @@ export const Course = (props) => {
 
     // daha iyisi ({title,description}) => ... yap
     return (
-        <>
-            <img src={image} alt='resim'></img>
-            <div>{title}</div>
-            <div>{description}</div>
-        </>
-  )
+      <>
+        <div className="card">
+          <div className="card-image">
+            <figure className="image is-4by3">
+              <img
+                src={image}
+                alt="Placeholder image"
+              />
+            </figure>
+          </div>
+          <div className="card-content">
+            <div className="media">
+              <div className="media-content">
+                <p className="title is-4">{title}</p>
+              </div>
+            </div>
+
+            <div className="content">
+              {description}
+            </div>
+          </div>
+        </div>
+      </>
+    );
 }
 
 
